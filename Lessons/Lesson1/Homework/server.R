3b1482e0b5fc47dbdc14bd3cfafe7f28c0a4b2f4
Peter Cvacho <peter.cvacho@zurich.com>
2018-04-16 12:06:16 +0200

    Homework 1

--- Lessons/Lesson1/Homework/server.R
+++ Lessons/Lesson1/Homework/server.R
@@ -1,6 +1,16 @@
-
-
+library(shiny)
+library(ggplot2)
+dt_KPI <- read.csv(".../Class1_Actuary/data/lesson2_KPI.csv")
+head(dt_KPI)
 # Define a server for the Shiny app
-function(input, output) {
+shinyServer(function(input, output) {
+  output$dt_KPIplot<- renderPlot({
+    g<-ggplot(data = dt_KPI,
+              mapping = aes_string(x = dt_KPI$Premium, y = dt_KPI$Expenses, colour = input$stlpec)
+    ) +
+      geom_point() +
+      geom_smooth()
+    g + labs(x="Premium",y="Expenses")
 
-}
+  })
+})
--- Lessons/Lesson1/Homework/ui.R
+++ Lessons/Lesson1/Homework/ui.R
@@ -1,23 +1,26 @@
-
-
-# Use a fluid Bootstrap layout
-fluidPage(    
-  
-  # Give the page a title
-  titlePanel("Lesson 1 - Homework"),
-  
-
-  sidebarLayout(      
-    
-
-    sidebarPanel(
-
-    ),
-    
-    
-    mainPanel(
-       
+library(shiny)
+library(datasets)
+dt_KPI <- read.csv(".../Class1_Actuary/data/lesson2_KPI.csv")
+# Define the overall UI
+shinyUI(
+  # Use a fluid Bootstrap layout
+  fluidPage(
+    # Give the page a title
+    titlePanel("Lesson 1 - Homework"),
+    # Generate a row with a sidebar
+    sidebarLayout(
+      # Define the sidebar with one input
+      sidebarPanel(
+        selectInput("stlpec", "Coloring var:",
+                    choices=c("Region","Unit","Segment","Business", "Year")),
+        #choices=c("Year")),
+        hr(),
+        helpText("Data from Lesson1 .")
+      ),
+      # Create a spot for the barplot
+      mainPanel(
+        plotOutput("dt_KPIplot")
+      )
     )
-    
   )
 )
