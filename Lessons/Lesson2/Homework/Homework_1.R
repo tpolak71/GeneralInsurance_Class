a414b0909df5d930553c5325ff7c888f4f4ffea9
tpolak71 <tomaspolakfmfi@gmail.com>
2018-04-28 01:03:58 +0200

    Homework 2

--- Lessons/Lesson2/Homework/Homework_1.R
+++ Lessons/Lesson2/Homework/Homework_1.R
@@ -3,32 +3,17 @@
 # __Commit__ it to your repository into `Lessons/Lesson2/Homework`.
 
 ## Code
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
 
+dt_KPI_raw %>%  mutate(uwr=Premium-Expenses-Losses) %>% 
+  group_by(Year) %>% 
+  summarize(uwr = sum(uwr, na.rm = TRUE)) %>%
+  arrange(uwr)%>%
+ggplot(aes(x = reorder(Year, uwr), y = uwr)) + 
+geom_col()
 
 
 
 # Your Explanation about analysis:
-# 
+# Worst year in terms of UWR was 2015 because of the worst performance (smallest number)
 # 
 # 
